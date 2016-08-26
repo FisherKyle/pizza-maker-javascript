a7d57d2fe366d02e2d6e52b9e57a03719d5a7ec3
Kyle Fisher <kyle@kylefisher.com>
2016-08-26 09:10:20 -0400

    establish forms for selecting size and pizza toppings.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
+// Backsies
+
+function Pizza(size, toppingOne, toppingTwo, toppingThree) {
+  this.size = size;
+  this.toppingOne = toppingOne;
+  this.toppingTwo = toppingTwo;
+  this.toppingThree = toppingThree;
+}
+
+function Delivery(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
+
+
+
 $(document).ready(function() {
   $("form#new-locale").submit(function(event) {
     event.preventDefault();
